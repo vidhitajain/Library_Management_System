7e80448d1dde9b9b6e35b8b9698cb317b677760a
vidhitajain <vidhitajain7@gmail.com>
2024-09-27 01:01:46 +0530

    Add test for viewing available books

--- test_library.py
+++ test_library.py
@@ -16,10 +16,20 @@ from library import Library, Book
 #     assert borrowed_book == book  # Check if the borrowed book is correct
 #     assert book not in library.books  # Book should be removed 
 
-def test_return_book():
+# def test_return_book():
+#     library = Library()
+#     book = Book("1034-4", "November 9", "Collen Hoover", 2015)
+#     library.add_book(book)
+#     library.borrow_book("1034-4")
+#     library.return_book(book)
+#     assert book in library.books  # Book should be available again
+
+def test_view_available_books():
     library = Library()
-    book = Book("1034-4", "November 9", "Collen Hoover", 2015)
-    library.add_book(book)
-    library.borrow_book("1034-4")
-    library.return_book(book)
-    assert book in library.books  # Book should be available again
+    book1 = Book("1034-4", "November 9", "Collen Hoover", 2015)
+    book2 = Book("3770-8", "my Gita", "Devdutt Pattanaik", 2016)
+    library.add_book(book1)
+    library.add_book(book2)
+    available_books = library.view_available_books()
+    assert book1 in available_books
+    assert book2 in available_books
