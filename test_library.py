2c27e73e0ffc919d00afccb62e468152492a13ce
vidhitajain <vidhitajain7@gmail.com>
2024-09-27 00:56:37 +0530

    Add test for returing books

--- test_library.py
+++ test_library.py
@@ -8,10 +8,18 @@ from library import Library, Book
 #     library.add_book(book)
 #     assert book in library.books  # Check if the book was added
 
-def test_borrow_book():
+# def test_borrow_book():
+#     library = Library()
+#     book = Book("1034-4", "November 9", "Collen Hoover", 2015)
+#     library.add_book(book)
+#     borrowed_book = library.borrow_book("1034-4")
+#     assert borrowed_book == book  # Check if the borrowed book is correct
+#     assert book not in library.books  # Book should be removed 
+
+def test_return_book():
     library = Library()
     book = Book("1034-4", "November 9", "Collen Hoover", 2015)
     library.add_book(book)
-    borrowed_book = library.borrow_book("1034-4")
-    assert borrowed_book == book  # Check if the borrowed book is correct
-    assert book not in library.books  # Book should be removed 
+    library.borrow_book("1034-4")
+    library.return_book(book)
+    assert book in library.books  # Book should be available again
