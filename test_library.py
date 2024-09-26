53514cf2df225e99a7c0e1bf8cc528772ea3647b
vidhitajain <vidhitajain7@gmail.com>
2024-09-27 00:53:35 +0530

    Add test for borrowng books

--- test_library.py
+++ test_library.py
@@ -2,8 +2,16 @@
 import pytest
 from library import Library, Book
 
-def test_add_book():
+# def test_add_book():
+#     library = Library()
+#     book = Book("1034-4", "November 9", "Collen Hoover", 2015)
+#     library.add_book(book)
+#     assert book in library.books  # Check if the book was added
+
+def test_borrow_book():
     library = Library()
     book = Book("1034-4", "November 9", "Collen Hoover", 2015)
     library.add_book(book)
-    assert book in library.books  # Check if the book was added
+    borrowed_book = library.borrow_book("1034-4")
+    assert borrowed_book == book  # Check if the borrowed book is correct
+    assert book not in library.books  # Book should be removed 
