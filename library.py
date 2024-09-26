15d7101f6b76be5a0f40d0ab80bcbd12a079299e
vidhitajain <vidhitajain7@gmail.com>
2024-09-27 00:55:12 +0530

    Implement borrow book function

--- library.py
+++ library.py
@@ -13,3 +13,10 @@ class Library:
 
     def add_book(self, book):
         self.books.append(book)
+
+    def borrow_book(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                self.books.remove(book)
+                return book
+        raise Exception("Book not available")
